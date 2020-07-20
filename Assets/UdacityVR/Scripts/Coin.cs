902f3fbe99e15bfd7e9cf90b9e2374ba4129c048
czhizhpon <czhizhpon@est.ups.edu.ec>
2020-07-19 23:46:09 -0500

    Animación de Coin

--- Assets/UdacityVR/Scripts/Coin.cs
+++ Assets/UdacityVR/Scripts/Coin.cs
@@ -4,14 +4,48 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour {
 
-	// TODO: Create variables to reference the game objects we need access to
-	// Declare a GameObject named 'coinPoofPrefab' and assign the 'CoinPoof' prefab to the field in Unity
+    // TODO: Create variables to reference the game objects we need access to
+    // Declare a GameObject named 'coinPoofPrefab' and assign the 'CoinPoof' prefab to the field in Unity
 
+    public GameObject coinPoofPrefab;
+    bool isClicked = false;
+    bool isMoving = true;
+    float y;
+    float pY;
+    float yInitial;
+    float currentTime;
 
-	void Update () {
-		// OPTIONAL-CHALLENGE: Animate the coin rotating
-		// TIP: You could use a method from the Transform class
-	}
+    void Start()
+    {
+        yInitial = transform.position.y;
+    }
+
+    void Update () {
+        // OPTIONAL-CHALLENGE: Animate the coin rotating
+        // TIP: You could use a method from the Transform class
+        
+        if (isClicked) {
+            y = 7 + (Mathf.Sin(Time.deltaTime));
+            
+            pY = yInitial + Mathf.Sin((Time.time - currentTime) * 7.0f) * 1.5f;
+            transform.Rotate(0.0f, y, 0.0f);
+            transform.position = new Vector3(transform.position.x, pY, transform.position.z);
+
+            if (pY < yInitial)
+            {
+                
+                createCoinPoof();
+                Object.Destroy(this.gameObject);
+                isClicked = false;
+            }
+
+        }
+        if (isMoving)
+        {
+            pY = yInitial + (Mathf.Sin(Time.time * 2.1f) * 0.07f);
+            transform.position = new Vector3(transform.position.x, pY, transform.position.z);
+        }
+    }
 
 
 	public void OnCoinClicked () {
@@ -23,8 +57,18 @@ public class Coin : MonoBehaviour {
 		// Prints to the console when the method is called
 		Debug.Log ("'Coin.OnCoinClicked()' was called");
 
-		// TODO: Display the poof effect and remove the coin from the scene
-		// Use Instantiate() to create a clone of the 'CoinPoof' prefab at this coin's position and with the 'CoinPoof' prefab's rotation
-		// Use Destroy() to delete the coin after for example 0.5 seconds
-	}
+        // TODO: Display the poof effect and remove the coin from the scene
+        // Use Instantiate() to create a clone of the 'CoinPoof' prefab at this coin's position and with the 'CoinPoof' prefab's rotation
+        // Use Destroy() to delete the coin after for example 0.5 seconds
+        this.isClicked = true;
+        this.isMoving = false;
+        this.yInitial = transform.position.y;
+        this.currentTime = Time.time;
+        
+    }  
+
+    private void createCoinPoof()
+    {
+        Object.Instantiate(coinPoofPrefab, transform.position, Quaternion.Euler(new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.y)));
+    }
 }
